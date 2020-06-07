345d58e957ed40fd2ba6c7b99c92b50febb12837
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2020-06-07 23:20:22 +0530

    Added Github Link

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, memo } from "react";
+import React, { useState, useEffect } from "react";
 import { getStory } from "../services/hackerNewsAPI";
 import {
   StoryWrapper,
