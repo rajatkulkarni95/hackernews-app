01fb9a3520e38cb9fa9326455c0024a724683ea7
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2020-06-07 23:17:21 +0530

    Added Git link

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import {
   GlobalStyle,
   StoriesContainerWrapper,
 } from "../styles/StoryContainerStyles";
+import { FaGithub } from "react-icons/fa";
 
 export const Header = () => {
   return (
@@ -10,6 +11,12 @@ export const Header = () => {
       <GlobalStyle />
       <StoriesContainerWrapper data-test-id="stories-container">
         <h1>Hacker News Stories</h1>
+        <h3>
+          Made by Rajat{" "}
+          <a href="https://github.com/rajatkulkarni95/hackernews-app">
+            <FaGithub />
+          </a>
+        </h3>
       </StoriesContainerWrapper>
     </>
   );
